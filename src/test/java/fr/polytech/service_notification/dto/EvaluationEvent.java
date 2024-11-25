e05168bc917f8e4e2161e44fd254c887f9cad57e
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-25 22:13:13 +0100

    Fix dto test

--- src/test/java/fr/polytech/service_notification/dto/EvaluationEvent.java
+++ src/test/java/fr/polytech/service_notification/dto/EvaluationEvent.java
@@ -13,13 +13,13 @@ class EvaluationEventTest {
         // Test des setters
         event.setEvaluationType("Performance");
         event.setUserId(123L);
-        event.setTarget("Project");
+        event.setTarget(2L);
         event.setMessage("Great work on the project!");
 
         // Test des getters avec AssertJ
         assertThat(event.getEvaluationType()).isEqualTo("Performance");
         assertThat(event.getUserId()).isEqualTo(123L);
-        assertThat(event.getTarget()).isEqualTo("Project");
+        assertThat(event.getTarget()).isEqualTo(2L);
         assertThat(event.getMessage()).isEqualTo("Great work on the project!");
     }
 
@@ -41,13 +41,13 @@ class EvaluationEventTest {
         EvaluationEvent event1 = new EvaluationEvent();
         event1.setEvaluationType("Feedback");
         event1.setUserId(456L);
-        event1.setTarget("Task");
+        event1.setTarget(2L);
         event1.setMessage("Needs improvement");
 
         EvaluationEvent event2 = new EvaluationEvent();
         event2.setEvaluationType("Feedback");
         event2.setUserId(456L);
-        event2.setTarget("Task");
+        event2.setTarget(2L);
         event2.setMessage("Needs improvement");
 
         // Vérification que les deux instances sont équivalentes
