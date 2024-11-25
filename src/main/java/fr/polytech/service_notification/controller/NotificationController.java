24bcddd05e73eac0f094996cbd20e0fab5e496e9
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-25 21:07:39 +0100

    Change mapping api

--- src/main/java/fr/polytech/service_notification/controller/NotificationController.java
+++ src/main/java/fr/polytech/service_notification/controller/NotificationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/notifications")
+@RequestMapping("/")
 public class NotificationController {
 
     @Autowired
