47b7947a6c4736361bcfb8e33ac269144137a274
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-25 20:27:32 +0100

    Creation du controller

---
+++ src/main/java/fr/polytech/service_notification/controller/NotificationController.java
@@ -0,0 +1,41 @@
+package fr.polytech.service_notification.controller;
+
+import fr.polytech.service_notification.model.Notification;
+import fr.polytech.service_notification.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    @Autowired
+    private NotificationService service;
+
+    @GetMapping
+    public List<Notification> getAllNotifications() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Notification getNotificationById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @GetMapping("/recipient/{recipientId}")
+    public Notification getNotificationByRecipientId(@PathVariable Long recipientId) {
+        return service.findByRecipientId(recipientId);
+    }
+
+    @PostMapping
+    public Notification createNotification(@RequestBody Notification notification) {
+        return service.save(notification);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNotification(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
